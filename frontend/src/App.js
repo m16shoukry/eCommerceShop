479889a06c0b0329897ec7bfd6f7611813c796ea
unknown <mohamedshokry921@gmail.com>
2021-04-20 10:14:56 +0200

    adding UI Screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+
 
 const App = () => {
   return (
-    <>
-     <h1>Welcome</h1>
-    </>
+    <Router>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <Route path='/' component={HomeScreen} exact />
+        <Route path='/product/:id' component={ProductScreen} />
+
+      </Container>
+    </main>
+    <Footer />
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Container, Nav, Navbar} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
+                <LinkContainer to='/'>
+                <Navbar.Brand>Shop</Navbar.Brand>
+                </LinkContainer>
+                
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                <LinkContainer to='/cart'>
+                <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/login'>
+                <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+                </LinkContainer>
+                </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
