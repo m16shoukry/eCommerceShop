19e4936905025a39e4e39bb85965cf2baf4a21bc
unknown <mohamedshokry921@gmail.com>
2021-05-07 08:07:42 +0200

    createUser with hashedPass

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,9 +38,41 @@ const getUserProfile = asyncHandler(async (req, res) => {
       res.status(404)
       throw new Error('User not found')
   }
- })
+})
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+  
+    const userExists = await User.findOne({ email })
+  
+    if (userExists) {
+      res.status(400)
+      throw new Error('User already exists')
+    }
+  
+    const user = await User.create({
+      name,
+      email,
+      password,
+    })
+  
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid user data')
+    }
+})
+  
 
 export {
     authUser,
-    getUserProfile
+    getUserProfile,
+    registerUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
 import { protect } from '../middleware/authMidleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile) 
+router.route('/profile').get(protect, getUserProfile)
+router.post('/', registerUser)
+
 
 export default router
