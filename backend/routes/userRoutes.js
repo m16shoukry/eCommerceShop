f0e50d96eabfadac6c1b2f51fa8260e3e73240a2
unknown <mohamedshokry921@gmail.com>
2021-05-09 00:39:35 +0200

    update user Endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMidleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 router.post('/', registerUser)
 
 
