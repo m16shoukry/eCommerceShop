406de1bd2c874f3ef24b7c58c694ee40d11d206c
unknown <mohamedshokry921@gmail.com>
2021-05-23 23:09:15 +0200

    getUserById&update  as admin Endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -114,11 +114,45 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if(user){
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+  
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 export {
     authUser,
     getUserProfile,
     registerUser,
     updateUserProfile,
     getUsers,
-    deleteUser
+    deleteUser,
+    updateUser,
+    getUserById,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,15 +6,27 @@ import {
     updateUserProfile,
     getUsers,
     deleteUser,
+    updateUser,
+    getUserById
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMidleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/').post(registerUser).get(protect, admin, getUsers)
-router.route('/:id').delete(protect, admin, deleteUser)
+
+router.route('/profile')
+.get(protect, getUserProfile)
+.put(protect, updateUserProfile)
+
+router.route('/')
+.post(registerUser)
+.get(protect, admin, getUsers)
+
+router.route('/:id')
+.delete(protect, admin, deleteUser)
+.get(protect, admin, getUserById)
+.put(protect, admin, updateUser)
 
 
 export default router
