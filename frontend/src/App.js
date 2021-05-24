bdb7128a948bf91933c0bdfe489f2f26d3d76e7c
unknown <mohamedshokry921@gmail.com>
2021-05-24 07:22:51 +0200

    EditProductScreen as Admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen.js'
 import UserListScreen from './screens/UserListScreen.js'
 import UserEditScreen from './screens/UserEditScreen.js'
 import ProductListScreen from './screens/ProductListScreen.js'
+import ProductEditScreen from './screens/ProductEditScreen.js'
 
 
 const App = () => {
@@ -33,6 +34,7 @@ const App = () => {
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
         <Route path='/admin/userlist' component={UserListScreen} />
+        <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
         <Route path='/admin/productlist' component={ProductListScreen} />
         <Route path='/admin/user/:id/edit' component={UserEditScreen} />
         <Route path='/' component={HomeScreen} exact />
