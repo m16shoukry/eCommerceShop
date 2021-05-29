51328ee166d479248b2d43b5ae3c75f87ccdc161
unknown <mohamedshokry921@gmail.com>
2021-05-29 06:56:28 +0200

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,20 @@ app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-app.get('/', (req, res) => {
-    res.send('API Is Running...')
-})
-
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
 app.use(notFound)
 app.use(errorHandler)
